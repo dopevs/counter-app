a26bf40186d04f6865a20193fbe1bfb5f41512f1
Wai Yan Win Htain <waiyanwinhtain2016@gmail.com>
2023-04-12 14:32:36 +0700

    feat: downgrade redis module version

--- index.js
+++ index.js
@@ -2,45 +2,42 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
-});
+const client = redis.createClient(process.env.REDIS_URL);
 
 app.get('/counter/:name', (req, res) => {
-  const { name } = req.params;
-  client.incr(name, (err, value) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.send(`Counter ${name} incremented to ${value}`);
-    }
+    const { name } = req.params;
+    client.incr(name, (err, value) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        res.send(`Counter ${name} incremented to ${value}`);
+      }
+    });
   });
-});
-
-app.get('/counters', (req, res) => {
-  client.keys('*', (err, keys) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      client.mget(keys, (err, values) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send('Internal Server Error');
-        } else {
-          const counters = keys.reduce((result, key, index) => {
-            result[key] = values[index];
-            return result;
-          }, {});
-          res.send(counters);
-        }
-      });
-    }
+  
+  app.get('/counters', (req, res) => {
+    client.keys('*', (err, keys) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+      } else {
+        client.mget(keys, (err, values) => {
+          if (err) {
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+          } else {
+            const counters = keys.reduce((result, key, index) => {
+              result[key] = values[index];
+              return result;
+            }, {});
+            res.send(counters);
+          }
+        });
+      }
+    });
   });
-});
-
-app.listen(3000, () => {
-  console.log('Counter app listening on port 3000');
-});
+  
+  app.listen(3000, () => {
+    console.log('Counter app listening on port 3000');
+  });
